61680fb12e35cd06cc4e92987265f318444c25f9
virat2729 <121380171+virat2729@users.noreply.github.com>
2022-12-26 00:05:04 +0530

    Update dsl_4.cpp

--- dsl_4.cpp
+++ dsl_4.cpp
@@ -1,104 +1,107 @@
 #include<iostream>
 using namespace std;
-class sort
+
+class sort 
 {
- float m[30];
- int i,j,n;
-public:
- void insert()
- {
-  cout<<"\nENTER NUMBER OF STUDENTS:"<<endl;
-  cin>>n;
-  cout<<"\nENTER MARKS:"<<endl;
-  for(i=0;i<n;i++)
-  {
-   cin>>m[i];
-  }
- }
- void display()
- {
-  cout<<"\nENTERED MARKS ARE:"<<endl;
-  for(i=0;i<n;i++)
-  cout<<m[i]<<endl;
- }
-
- void bubble()
- {
-  float temp;
-  for(i=0;i<n-1 ;i++)
-  {
-   for(j=0;j<(n-1)-i;j++)
-   {
-    if(m[j]>m[j+1])
+    float arr[30];
+    int i,j,n;
+    public:
+    void insert()
     {
-     temp=m[j];
-     m[j]=m[j+1];
-     m[j+1]=temp;
+        cout<<"\nEnter number of students: ";
+        cin>>n;
+        cout<<"\nEnter marks of "<<n<<" students";
+        for(i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
     }
-   }
-  }
-cout<<"TOP FIVE:"<<endl;
-  for(i=n-1;i>=0;i--)
-  {
-   cout<<m[i]<<endl;
 
-  }
- }
- void selection()
- {
-  int min;
-  float temp;
-  for(i=0;i<(n-1);i++)
-  {
-   min=i;
-   for(j=i+1;j<n;j++)
-   {
-    if(m[j]<m[min])
+    void display()
     {
-     min=j;
+        cout<<"\nMarks of "<<n<<" students are: ";
+        for(i=0;i<n;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
     }
-   }
-    temp=m[i];
-    m[i]=m[min];
-    m[min]=temp;
 
-  }
-  cout<<"TOP FIVE:"<<endl;
-  for(i=n-1;i>=0;i--)
-  {
-   cout<<m[i]<<endl;
-  }
- }
-};
+    void bubble()
+    {
+        float temp;
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+        cout<<"\nTop five marks are: ";
+        for(i=n-1;i>=0;i--)
+        {
+            cout<<arr[i]<<" ";
+        }
+    }
+
+    void selection()
+    {
+        int min;
+        float temp;
+        for(i=0;i<n;i++)
+        {
+            min=i;
+            for(j=i+1;j<n;j++)
+            {
+                if(arr[j]<arr[min])
+                {
+                    min=j;
+                }
+            }
+            temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+        cout<<"\nTop five marks are: ";
+        for(i=n-1;i>=0;i--)
+        {
+            cout<<arr[i]<<" ";
+        }
+    }
+}s;
+
 int main()
 {
- sort s;
- int ch,x;
- cout<<"LIST OF TOP FIVE STUDENTS:"<<endl;
- 
- cout<<"\nInsert elemets in array!!!";
- s.insert();
+    int ch;
+    cout<<"\nInsert information: ";
+    s.insert();
 do{
- cout<<"\n1.DISPLAY\n2.TOP FIVE BY BUBBLE SORT\n3.TOP FIVE BY SELECTION SORT\n4,EXIT\nENTER CHOICE:"<<endl;
-  cin>>ch;
+    cout<<"\n1.Display";
+    cout<<"\n2.Top five by bubble sort";
+    cout<<"\n3.Top five by selection sort";
+    cout<<"\n4.Exit";
 
-  switch(ch)
-  {
-    case 1:
-    s.display();
-    break;
+    cout<<"\nEnter your choice.";
+    cin>>ch;
 
-    case 2:
-    s.bubble();
-    break;
+    switch(ch)
+    {
+        case 1:
+        s.display();
+        break;
 
-    case 3:
-    s.selection();
-    break;
-  }
- cout<<"\nDo you want to continue?\n1.YES\n2.NO";
-  cin>>x;
-}while(x==1);
+        case 2:
+        s.bubble();
+        break;
 
- return 0;
+        case 3:
+        s.selection();
+        break;
+    }
+}while(ch!=4);
 }
